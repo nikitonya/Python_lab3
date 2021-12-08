c9c0d32905215edd289efcffc5085017dfa5aac1
nikitonya <nik_osipov_2014@mail.ru>
2021-12-08 23:08:04 +0400

    add serialization and deseralization

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # пузырьком + yaml
 import json
+import yaml
 from tqdm import tqdm
 
 
-def bubble_sort(arr: list):
-    for i in range(len(arr) - 1):
-        for j in range(len(arr) - 1 - i):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print(arr)
-
-
-class SortPeople:
+class ValidPeople:
     data: list
 
     def __init__(self, path) -> None:
@@ -19,23 +12,36 @@ class SortPeople:
 
 
 def bubblesort(data: list, flag: str) -> list:
-    with tqdm(range(len(data) - 1)) as pbar:
+    with tqdm(range(len(data) - 1), colour="Blue", ncols = 150) as pbar:
         for j in range(len(data) - 1):
             for i in range(len(data) - 1 - j):
-                # print(f"Сейчас сравниваются {data[i][flag]} от {data[i]['email']} и {data[i + 1][flag]} от {data[i+1]['email']}")
-                if float(data[i][flag]) > float(data[i + 1][flag]):
-                    # print("Смена")
+                if str(data[i][flag]) > str(data[i + 1][flag]):
                     data[i], data[i + 1] = data[i + 1], data[i]
             pbar.update(1)
     return data
 
 
-valid_data = SortPeople("C:\\Users\\nikit\\PycharmProjects\\python_lab3\\output.txt")
-sort_data = bubblesort(valid_data.data[0:100], 'height')
+def write_yaml(sort_data, path):
+    with open(path, 'w') as write_file:
+        yaml.dump(sort_data, write_file)
+
+
+def read_yaml(path):
+    with open(path, 'r') as read_file:
+        return yaml.safe_load(read_file)
+
+
+valid_data = ValidPeople("C:\\Users\\nikit\\PycharmProjects\\python_lab3\\output.txt")
+sort_data = bubblesort(valid_data.data, "height")
 
 json.dump(
     sort_data,
     open(
         "C:\\Users\\nikit\\PycharmProjects\\python_lab3\\check.txt",
         "w",
-        encoding="windows-1251"), indent=5, ensure_ascii=False,)
+        encoding="windows-1251"), indent=5, ensure_ascii=False)
+
+write_yaml(sort_data, 'serialization.yaml')
+read = read_yaml('serialization_all.yaml')
+
+print(read)
